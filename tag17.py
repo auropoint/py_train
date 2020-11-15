e1314fe27b734e54ea239e9118b985c9bef0c5f1
auropoint <auropoint@gmail.com>
2020-11-15 15:33:21 +0530

    Выделена фикстура и осуществлён переход на py.test

---
+++ tag17.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest
+from group import Group
+
+class Tab(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.base_url = "https://www.google.com/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+
+    def test_tab(self):
+        driver = self.driver
+        self.login(driver, username="admin", password="secret")
+        self.create_group(driver, Group(name="g", header="gh", footer="gf"))
+        self.logout(driver)
+
+    def test_tab_empty(self):
+        driver = self.driver
+        self.login(driver, username="admin", password="secret")
+        self.create_group(driver, Group(name="", header="", footer=""))
+        self.logout(driver)
+
+    def logout(self, driver):
+        driver.find_element_by_link_text(u"Выйти").click()
+
+    def return_to_groups_page(self, driver):
+        driver.find_element_by_link_text("group page").click()
+
+    def create_group(self, driver, group):
+        self.open_groups_page(driver)
+        # init group creation
+        driver.find_element_by_name("new").click()
+        # fill group form
+        driver.find_element_by_name("group_name").click()
+        driver.find_element_by_name("group_name").clear()
+        driver.find_element_by_name("group_name").send_keys(group.name)
+        driver.find_element_by_name("group_header").click()
+        driver.find_element_by_name("group_header").clear()
+        driver.find_element_by_name("group_header").send_keys(group.header)
+        driver.find_element_by_name("group_footer").click()
+        driver.find_element_by_name("group_footer").clear()
+        driver.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit group creation
+        driver.find_element_by_name("submit").click()
+        self.return_to_groups_page(driver)
+
+    def open_groups_page(self, driver):
+        driver.find_element_by_link_text(u"Группы").click()
+
+    def login(self, driver, username, password):
+        self.open_home_page(driver)
+        driver.find_element_by_name("user").click()
+        driver.find_element_by_name("user").clear()
+        driver.find_element_by_name("user").send_keys(username)
+        driver.find_element_by_name("pass").clear()
+        driver.find_element_by_name("pass").send_keys(password)
+        driver.find_element_by_xpath(u"//input[@value='Войти']").click()
+
+    def open_home_page(self, driver):
+        driver.get("http://localhost/addressbook/")
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+    def is_alert_present(self):
+        try:
+            self.driver.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally:
+            self.accept_next_alert = True
+
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+
+if __name__ == "__main__":
+    unittest.main()
