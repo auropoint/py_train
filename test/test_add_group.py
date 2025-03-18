5a80b190da529c14427376d55e98f75f8bcadcd8
auropoint <auropoint@gmail.com>
2025-03-18 13:34:51 +0700

    2.4. conftest.py added: to store fixture visible for pytest from any test file

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,13 +1,4 @@
-import pytest
 from model.group import Group
-from fixture.application import Application
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_add_group(app):
@@ -20,4 +11,3 @@ def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
-
