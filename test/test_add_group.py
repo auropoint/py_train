ce9e4f1b2f0a66bfe61d7982035bc57f9725cda1
auropoint <auropoint@gmail.com>
2025-03-21 13:26:08 +0700

    3.4. data preconditions ensure/creation in tests (creation of an entity before the test of its deletion)

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,7 +3,6 @@ from model.group import Group
 
 def test_add_group(app):
     app.group.create(Group(name="dfgdfg", header="dfgdfg", footer="dfgfghgfhg"))
-    # app.session.logout()
 
 
 def test_add_empty_group(app):
